b1dfec88881beb1079b617e24366bb72eafe7576
itamarzil123 <itamarzil123@hotmail.com>
2019-03-25 12:59:06 +0200

    installing postgres, testing connection after creating authdb database locally in pgadmin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
@@ -8,6 +9,23 @@ const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
     console.log('server listening on port:', PORT);
 } )
+
+// connection string
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/authdb');
+
+
+// testing connection to local postgres
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+// router
 app.get('/', (req, res) => {
     console.log('server running on port:', PORT);
 })
