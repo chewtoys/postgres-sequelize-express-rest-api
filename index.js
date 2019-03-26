f37018be60081831bf6ce3d2340876d0c62fadb9
itamarzil123 <itamarzil123@hotmail.com>
2019-03-26 15:30:15 +0200

    added passport setup file

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 const parseurl = require("parseurl");
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 app.listen(PORT, () => {
   console.log("server listening on port:", PORT);
