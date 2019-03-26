eff836c84bf56982ae67af18ca5425fe5b1d66cd
itamarzil123 <itamarzil123@hotmail.com>
2019-03-26 14:57:27 +0200

    login done

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,14 +31,29 @@ class AuthController {
   }
 
   static login(req, res, next) {
-    if (isValidUser(req.body)) {
-      // should use a query from /queries folder that is responsible for creating users
-      res.json({
-        msg: "login"
-      });
-    } else {
-      next(new Error("Invalid User"));
+    // username, email, password validators
+    console.log("req.body:", req.body);
+    if (!req.body.username) {
+      res.json({ error: "Username cannot be empty." });
     }
+
+    const userData = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    User.findOne({
+      where: { email: req.body.email }
+    }).then(user => {
+      if (!user) {
+        console.log("no user found");
+        res.json({ error: "No User Found" });
+      } else {
+        console.log("user exists");
+      }
+      res.json(userData);
+    });
   }
 }
 
