e4eb13c86dc61e969d1ee733897b0d6e837b19f3
itamarzil123 <itamarzil123@hotmail.com>
2019-03-28 12:53:15 +0200

    fixed typo

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
 
-// *** Routers *** //
-const authRouter = require("./router/auth-router");
-const questionRouter = require("./router/question-router");
-
-const session = require("express-session");
-const PORT = process.env.PORT || 3000;
-const parseurl = require("parseurl");
-const passport = require("passport");
 const initPassport = require("./initPassport");
 const initParsers = require("./initParsers");
 const initRouters = require("./initRouters");
 const initExpressSession = require("./initExpressSession");
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("server listening on port:", PORT);
